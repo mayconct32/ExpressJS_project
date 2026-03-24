d13a74046fadf9d6e9418f174c9ba3aa3a91e6a1
mayconct32 <maycon326754@gmail.com>
2026-03-23 22:15:07 -0300

    feat: adiciona novas rotas

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
-const express = require("express");
+import express from "express";
 
 const app = express();
 
+const port = 8081;
+
+app.get("/", (req, res) => {
+    res.send("seja Bem-vindo")
+});
+
+app.get("/teste", (req, res) => {
+    res.send("teste")
+});
+
 app.listen(
-    8081,
-    function(){
-        console.log("o servidor esta rodando...")
+    port,
+    () => {
+        console.log(`o servidor esta rodando na porta ${port}...`)
     }
 )
