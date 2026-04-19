e571df85ed430e86a5bf30240dc4bfb99d467d6b
mayconct32 <maycon326754@gmail.com>
2026-04-19 20:31:26 -0300

    refactor(validators): aplicar ISP separando validadores para criação e atualização

--- src/express/middlewares/express_validator.js
+++ src/express/middlewares/express_validator.js
@@ -1,36 +1,17 @@
 import { body, param } from "express-validator"
-import { users_model } from "../../models/users_model.js"
 import { isValidObjectId } from "mongoose"
 
 
-export const body_validator = [
+export const create_user_body_validator = [
     body("username").notEmpty().trim().withMessage(
         "Requires username"
     ).isLength({min: 5}).withMessage(
         "The username must be at least 5 characters long"
-    ).custom(
-        async username => {
-            const conflict_user = await users_model.findOne(
-                {username: username}
-            )
-            if (conflict_user){
-                throw new Error("this username is already in use")
-            }
-        }
     ),
     body("email").notEmpty().trim().withMessage(
         "Requires email"
     ).isEmail().withMessage(
         "This is not a valid email"
-    ).custom(
-        async (email) => {
-            const conflict_user = await users_model.findOne(
-                {email: email}
-            )
-            if (conflict_user){
-                throw new Error("this email is already in use")
-            }
-        }
     ),
     body("password").notEmpty().trim().withMessage(
         "Requires password"
@@ -39,13 +20,21 @@ export const body_validator = [
     )
 ]
 
-export const param_validator = param("user_id").custom(async user_id => {
+export const update_user_body_validator = [
+    body("username").optional().trim().isLength({min: 5}).withMessage(
+        "The username must be at least 5 characters long"
+    ),
+    body("email").optional().trim().isEmail().withMessage(
+        "This is not a valid email"
+    ),
+    body("password").optional().trim().isLength({min: 8}).withMessage(
+        "The password must be at least 8 characters long"
+    )
+]
+
+export const user_id_param_validator = param("user_id").custom(user_id => {
     if(!isValidObjectId(user_id)){
         throw new Error("this ID is invalid")
-    } 
-    const user = await users_model.findOne({_id: user_id})
-    if (!user){
-        throw new Error("This user does not exist")
     }
 })
 
--- src/express/routers/users.js
+++ src/express/routers/users.js
@@ -1,8 +1,9 @@
 import express from "express"
 import { UsersController } from "../controllers/users_controller.js"
 import { 
-    body_validator,
-    param_validator
+    create_user_body_validator,
+    update_user_body_validator,
+    user_id_param_validator
 } from "../middlewares/express_validator.js"
 import { validation_handler } from "../middlewares/error_handlers.js"
 import { verify_permission } from "../middlewares/auth.js"
@@ -13,14 +14,14 @@ const users_controller = new UsersController()
 
 router.post(
     "/", 
-    body_validator, 
+    create_user_body_validator, 
     validation_handler,
     users_controller.create_user
 )
 
 router.get(
     "/:user_id", 
-    param_validator, 
+    user_id_param_validator, 
     validation_handler,
     verify_permission,
     users_controller.get_user
@@ -28,8 +29,8 @@ router.get(
 
 router.put(
     "/:user_id",
-    param_validator,
-    body_validator,
+    user_id_param_validator,
+    update_user_body_validator,
     validation_handler,
     verify_permission,
     users_controller.update_user
@@ -37,7 +38,7 @@ router.put(
 
 router.delete(
     "/:user_id", 
-    param_validator, 
+    user_id_param_validator, 
     validation_handler,
     verify_permission,
     users_controller.delete_user
