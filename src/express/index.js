8d9772a68b3fe8c528336a9600183debfe3fa8ca
mayconct32 <maycon326754@gmail.com>
2026-04-22 14:36:35 -0300

    feat(server): adiciona tratamento de erros na inicialização do servidor

--- src/express/index.js
+++ src/express/index.js
@@ -2,19 +2,21 @@ import { mongodb_connection } from "../db_connection.js"
 import { server_http } from "./socket_io.js"
 
 
-async function start_server(){
-    try{
-        await mongodb_connection()
-        server_http.listen(
-            3000, 
-            () => {
-                console.log("Server is running on PORT 3000")
-            }
-        )
-    } catch (error){
-        console.error("Failed to connect to MongoDB", error)
-        process.exit(1)
-    }
+async function start_server() {
+  try {
+    await mongodb_connection();
+  } catch (error) {
+    console.error("Failed to connect to MongoDB", error);
+    process.exit(1);
+  }
+  try {
+    server_http.listen(3000, () => {
+      console.log("Server is running on PORT 3000");
+    });
+  } catch (error) {
+    console.error("Failed to start server", error);
+    process.exit(1);
+  }
 }
 
 start_server()
