9371d4df83ab5eae3dd25cb72eff17ad53c01cc8
mayconct32 <maycon326754@gmail.com>
2026-05-03 11:32:26 -0300

    test(auth): Add test to POST /auth

--- tests/jest.setup.js
+++ tests/jest.setup.js
@@ -1,8 +1,12 @@
-import { MongoMemoryServer } from "mongodb-memory-server";
-import { mongodb_connection } from "../src/db_connection";
-import { users_model } from "../src/models/users_model";
-import mongoose from "mongoose";
-import { authService } from "../src/services/auth_service";
+import { MongoMemoryServer } from "mongodb-memory-server"
+import { mongodb_connection } from "../src/db_connection"
+import { users_model } from "../src/models/users_model"
+import mongoose from "mongoose"
+import { authService } from "../src/services/auth_service"
+import { response } from "express"
+import request from "supertest"
+import { app } from "../src/express/app"
+import { encrypt_password } from "../src/services/encryption"
 
 
 let mongod
@@ -15,29 +19,34 @@ beforeAll(async () =>
     }
 )
 
-beforeEach(async () => {
-    user1 = {
-        username: "userexamplee",
-        email: "userr@example.com",
-        password: "test123123123"
+beforeEach(async () => 
+    {
+        user1 = {
+            username: "userexamplee",
+            email: "userr@example.com",
+            password: await encrypt_password("test123123123")
+        }
+        user2 = {
+            username: "userexample2",
+            email: "user2@example.com",
+            password: "test123123123"
+        }
+        const response = await users_model.create([user1, user2])
+        user = response[0]
+        const user_token = authService.build_user_token_payload(user)
+        fake_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNjlmNjZkN2EwYWNhMzI1YzAyMzBkMDdiIiwidXNlcm5hbWUiOiJtYXljb25sb2JvIiwiZW1haWwiOiJtYXljb25AZ21haWwuY29tIn0._3GV1cJQXyYub0DIdNcbYP3Z2rntwKqA2X0R5ArmYbk"
+        token = authService.coding_token(user_token)
     }
-    user2 = {
-        username: "userexample2",
-        email: "user2@example.com",
-        password: "test123123123"
-    }
-    response = await users_model.create([user1, user2])
-    user = response[0]
-    const user_token = authService.build_user_token_payload(user)
-    token = authService.coding_token(user_token)
-})
+)
 
-afterEach(async () => {
-    const collections = await mongoose.connection.db.collections()
-    for (const collection of collections) {
-        await collection.deleteMany({})
+afterEach(async () => 
+    {
+        const collections = await mongoose.connection.db.collections()
+        for (const collection of collections) {
+            await collection.deleteMany({})
+        }
     }
-})
+)
 
 afterAll(async () => 
     {
